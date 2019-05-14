c0eee8831f097f79e98952f7a1393c86792a161e
baantyy <biswajeetbanty@gmail.com>
2019-05-15 00:53:47 +0530

    db-config

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 mongoose.Promise = global.Promise
-//const db = 'mongodb://localhost:27017/chat-app'
-const db = 'mongodb://banty:qwe123@ds149706.mlab.com:49706/heroku_q9zqqjvz'
+const db = 'mongodb://localhost:27017/chat-app'
+//const db = 'mongodb://banty:qwe123@ds149706.mlab.com:49706/heroku_q9zqqjvz'
 mongoose.connect(db,{
         useNewUrlParser: true,
         useCreateIndex: true
